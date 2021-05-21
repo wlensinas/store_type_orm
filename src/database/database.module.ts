2067d7237ca0f7578ced6f3f66973682d4c2e5e6
Walter Lensinas <wlensinas@buenosaires.gob.ar>
2021-05-21 00:34:24 -0300

    added config on database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,18 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'my_db',
+  password: '123456',
+  port: 5432,
+});
+
+client.connect();
 
 @Global()
 @Module({
@@ -10,7 +21,11 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
