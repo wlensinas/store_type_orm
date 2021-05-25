2c073d90f007b9fd8c7a18bd688238a034e86432
Walter Lensinas <wlensinas@gmail.com>
2021-05-25 20:01:49 -0300

    added findone and create relations for products and categories

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -5,12 +5,16 @@ import { Repository } from 'typeorm';
 import { Product } from './../entities/product.entity';
 import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
 
-import { BrandsService } from './brands.service';
+//import { BrandsService } from './brands.service';
+import { Category } from './../entities/category.entity';
+import { Brand } from './../entities/brand.entity';
+
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectRepository(Product) private productRepo: Repository<Product>,
-    private brandService: BrandsService,
+    @InjectRepository(Brand) private brandRepo: Repository<Brand>,
+    @InjectRepository(Category) private categoryRepo: Repository<Category>,
   ) {}
 
   findAll() {
@@ -21,7 +25,7 @@ export class ProductsService {
 
   async findOne(id: number) {
     const product = await this.productRepo.findOne({
-      relations: ['brand'],
+      relations: ['brand', 'categories'],
       where: {
         id,
       },
@@ -44,8 +48,12 @@ export class ProductsService {
     // creando en base al DTO
     const newProduct = await this.productRepo.create(data);
     if (data.brandId) {
-      const brand = await this.brandService.findOne(data.brandId);
+      const brand = await this.brandRepo.findOne(data.brandId);
       newProduct.brand = brand;
+    }
+    if (data.categoriesIds) {
+      const categories = await this.categoryRepo.findByIds(data.categoriesIds);
+      newProduct.categories = categories;
     }
     return this.productRepo.save(newProduct);
   }
@@ -53,7 +61,7 @@ export class ProductsService {
   async update(id: number, changes: UpdateProductDto) {
     const product = await this.productRepo.findOne(id);
     if (changes.brandId) {
-      const brand = await this.brandService.findOne(changes.brandId);
+      const brand = await this.brandRepo.findOne(changes.brandId);
       product.brand = brand;
     }
     this.productRepo.merge(product, changes);
